5153efa7bdf49bbd03b8e1863f74ab771132b906
wrathchild14 <jovanprodanov123@gmail.com>
2022-09-06 19:22:55 +0100

    Create car agent script and tag appropriate assets

---
+++ Project/Assets/MLCar/Scripts/CarAgent.cs
@@ -0,0 +1,19 @@
+using Unity.MLAgents;
+
+namespace MLCar.Scripts
+{
+    public class CarAgent : Agent
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
