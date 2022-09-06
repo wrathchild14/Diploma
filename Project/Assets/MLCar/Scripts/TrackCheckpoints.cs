3ed7376c015da2e490f9ada4ae50ba6bafbb0b38
wrathchild14 <jovanprodanov123@gmail.com>
2022-09-06 20:36:35 +0100

    Improve TrackCheckpoints class to handle events

--- Project/Assets/MLCar/Scripts/TrackCheckpoints.cs
+++ Project/Assets/MLCar/Scripts/TrackCheckpoints.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,9 @@ namespace MLCar.Scripts
 {
     public class TrackCheckpoints : MonoBehaviour
     {
+        public event EventHandler<CarCheckpointEventArgs> OnCarCorrectCheckpoint;
+        public event EventHandler<CarCheckpointEventArgs> OnCarWrongCheckpoint;
+
         [SerializeField] private List<Transform> carTransformList;
         private List<CheckpointSingle> _checkpointSingleList;
         private List<int> _nextCheckpointSingleIndexList;
@@ -23,7 +27,6 @@ namespace MLCar.Scripts
             _nextCheckpointSingleIndexList = new List<int>();
             foreach (var unused in carTransformList)
             {
-                Debug.Log(unused);
                 _nextCheckpointSingleIndexList.Add(0);
             }
         }
@@ -38,11 +41,36 @@ namespace MLCar.Scripts
                 Debug.Log("Correct checkpoint: " + checkpointSingle.name);
                 _nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform.parent)] =
                     (nextCheckpointSingleIndex + 1) % _checkpointSingleList.Count; // For multiple laps
+                OnCarCorrectCheckpoint?.Invoke(this, new CarCheckpointEventArgs(carTransform));
             }
             else
             {
                 Debug.Log("Wrong checkpoint");
+                OnCarWrongCheckpoint?.Invoke(this, new CarCheckpointEventArgs(carTransform));
             }
+        }
+
+        public class CarCheckpointEventArgs : EventArgs
+        {
+            public readonly Transform CarTransform;
+
+            public CarCheckpointEventArgs(Transform carTransform)
+            {
+                CarTransform = carTransform;
+            }
+        }
+
+        public void ResetCheckpoint(Transform carTransform)
+        {
+            _nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform.parent)] = 0;
+        }
+
+        public CheckpointSingle GetNextCheckpoint(Transform carTransform)
+        {
+            var checkpoint =
+                _checkpointSingleList[_nextCheckpointSingleIndexList[carTransformList.IndexOf(carTransform.parent)]];
+            // Its heavy, but used in observation collection
+            return checkpoint;
         }
     }
 }
